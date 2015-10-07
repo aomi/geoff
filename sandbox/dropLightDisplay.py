8777bc7e836d60f254bb3c57ff06d881fffca5d5
smithaustin <smith.austin149@gmail.com>
2015-10-07 16:11:10 -0700

    Version 1.0
    needs actual functions when receiving angle data. currently just sudo code

--- sandbox/dropLightDisplay.py
+++ sandbox/dropLightDisplay.py
@@ -1,1 +1,23 @@
+from sense_hat import SenseHat
+import time
+sense = SenseHat()
 
+#x and y coords
+x = 0 
+y = 0
+
+while(1)
+  #Dont know the function to grab the stuff
+  getAngleOfX
+  getAngleOfY
+  clamp(x,-89,89)
+  clamp(y,-89,89)
+  #maps x,y to nearest int between 0,8
+  x = int(x/11.25)
+  y = int(y/11.25)
+  sense.set_pixel(x,y,0,0,255)
+  time.sleep(0.1)
+
+#restrains value between minn and maxn
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
