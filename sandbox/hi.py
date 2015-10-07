68acda1083417c1af2e2f91a73f6bbb30e1ece63
Aomi Jokoji <aomijokoji@gmail.com>
2015-10-06 17:30:02 -0700

    the more the better

--- sandbox/hi.py
+++ sandbox/hi.py
@@ -1,2 +1,6 @@
 #this is getting far too wild. I am using this text editor called "atom" and so far I am liking it somewhat.
 print("Hello World")
+swag = "hello world"
+def hello(insult):
+    print("You're such a " + insult)
+hello("baddy")
