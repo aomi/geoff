43fbbe466d2ec5673fd85d41d7033dcb65f2dce7
smithaustin <smith.austin149@gmail.com>
2015-10-21 13:55:12 -0700

    UNFINSHED need to update while loop

--- sandbox/xlsvTest.py
+++ sandbox/xlsvTest.py
@@ -1,7 +1,19 @@
 import xlsxwriter
+from w1thermsensor import W1ThermSensor
+from datetime import datetime
 
 workbook = xlsxwriter.Workbook('test.xlsx')
-worksheet = workbook.add_worksheet()
+worksheet = workbook.add_worksheet("Temperature Data")
+bold = workbook.add_format({"bold":True})
+
+sensor = W1ThermSensor()
+
+worksheet.write("A1", "Time", bold)
+worksheet.write("A2", "Temp Values", bold)
+try:
+    while True:
+        temp = sensor.get_temperature()
+
 
 worksheet.write('A1', 'Sup maaaaan')
 
