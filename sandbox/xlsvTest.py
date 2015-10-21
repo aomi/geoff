1518ee62f3fedcc7741ee0b7b6253ee25306d576
smithaustin <smith.austin149@gmail.com>
2015-10-21 14:59:35 -0700

    more updates

--- sandbox/xlsvTest.py
+++ sandbox/xlsvTest.py
@@ -10,11 +10,12 @@ sensor = W1ThermSensor()
 
 worksheet.write("A1", "Time", bold)
 worksheet.write("A2", "Temp Values", bold)
+row = 1
 try:
     while True:
         temp = sensor.get_temperature()
-
-
+        time = datetime.time()
+        worksheet.write(row, 0, temp)
 worksheet.write('A1', 'Sup maaaaan')
 
 workbook.close()
