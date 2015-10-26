5602611b925975d376d32e92e3141404a355592c
smithaustin <smith.austin149@gmail.com>
2015-10-26 09:19:24 -0700

    debugging 1

--- sandbox/xlsvTest.py
+++ sandbox/xlsvTest.py
@@ -2,7 +2,7 @@ import xlsxwriter
 from w1thermsensor import W1ThermSensor
 from datetime import datetime
 
-name = raw_input("Name of Document?")
+name = raw_input("Name of Document? ")
 
 workbook = xlsxwriter.Workbook(str(name)+'.xlsx')
 worksheet = workbook.add_worksheet("Temperature Data")
@@ -19,7 +19,8 @@ try:
         time = datetime.now()
         worksheet.write(row, 0, time)
         worksheet.write(row, 1, temp)
+        row += 1
 
 except KeyboardInterrupt:
-    print("Finished")
+    print(" Finished")
 workbook.close()
