686938cbc44ecd836687445d202305e3bda657bb
Aomi <aomijokoji@gmail.com>
2015-11-24 16:49:25 -0800

    added xlsx functionality

--- sandbox/testSensors.py
+++ sandbox/testSensors.py
@@ -1,23 +1,51 @@
+
+import xlsxwriter
 from w1thermsensor import W1ThermSensor
-
+from datetime import datetime
 from Adafruit_BME280 import *
+import time
 
-sensor = BME280(mode=BME280_OSAMPLE_8)
+name = raw_input("Name of Document? ")
 
-degrees = sensor.read_temperature()
-pascals = sensor.read_pressure()
-hectopascals = pascals / 100
-humidity = sensor.read_humidity()
+workbook = xlsxwriter.Workbook(str(name)+'.xlsx')
+worksheet = workbook.add_worksheet("Temperature Data")
+bold = workbook.add_format({"bold":True})
 
-temp_sensor = W1ThermSensor()
-temperature_in_celsius = temp_sensor.get_temperature()
-temperature_in_fahrenheit = temp_sensor.get_temperature(W1ThermSensor.DEGREES_F)
-temperature_in_all_units = temp_sensor.get_temperatures([W1ThermSensor.DEGREES_C, W1ThermSensor.DEGREES_F, W1ThermSensor.KELVIN])
+bme_sensor = BME280(mode=BME280_OSAMPLE_8)
+water_temp_sensor = W1ThermSensor()
 
-while True:
-    temperature_in_celsius = temp_sensor.get_temperature()
-    print ('Timestamp = {0:0.3f}'.format(sensor.t_fine))
-    print ('Temp      = {0:0.3f} deg C'.format(degrees))
-    print ('Pressure  = {0:0.2f} hPa'.format(hectopascals))
-    print ('Humidity  = {0:0.2f} %'.format(humidity))
-    print("water temp = " + temperature_in_celsius)
+worksheet.write("A1", "Time", bold)
+worksheet.write("A2", "Air_Temp Values (C)", bold)
+worksheet.write("A3", "Pressure Values (hPa)", bold)
+worksheet.write("A4", "Humidity Values (%)", bold)
+worksheet.write("A5", "Water_Temp Values (C)", bold)
+row = 1
+
+try:
+    while True:
+        
+        air_temp     = bme_sensor.read_temperature()
+        pascals      = bme_sensor.read_pressure()
+        hectopascals = pascals / 100
+        humidity     = bme_sensor.read_humidity()
+        water_temp   = water_temp_sensor.get_temperature()
+        time = datetime.now()
+        
+        worksheet.write(row, 0, time)
+        worksheet.write(row, 1, air_temp)
+        worksheet.write(row, 2, hectopascals)
+        worksheet.write(row, 3, humidity)
+        worksheet.write(row, 4, water_temp)
+        row += 1
+        
+        print ('Timestamp = {0:0.3f}'.format(bme_sensor.t_fine))
+        print ('Air_Temp  = {0:0.3f} deg C'.format(air_temp))
+        print ('Pressure  = {0:0.2f} hPa'.format(hectopascals))
+        print ('Humidity  = {0:0.2f} %'.format(humidity))
+        print ("Water_Temp = " + str(water_temp) + " deg C")
+        print("-------------------------------------------------")
+        time.sleep(1)
+        
+except KeyboardInterrupt:
+    print(" Finished")
+workbook.close()
