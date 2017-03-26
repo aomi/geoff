3570227805e5b7bd42788429557a48a72a72e8fa
Aomi <aomijokoji@gmail.com>
2017-03-25 22:54:29 -0700

    Remove virtualenv files

--- sandbox/hi.py
+++
@@ -1,6 +0,0 @@
-#this is getting far too wild. I am using this text editor called "atom" and so far I am liking it somewhat.
-print("Hello World")
-swag = "hello world"
-def hello(insult):
-    print("You're such a " + insult)
-hello("baddy")
